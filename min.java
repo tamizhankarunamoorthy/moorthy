bc2c6acf422f6b5241f462f715004ac97ef6e074
tamizhankarunamoorthy <karunamoorthyajith@gmail.com>
2016-08-16 18:50:49 +0530

    Create min.java

---
+++ min.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class min
+{
+ public static void main(String args[])
+ {
+  Scanner sc=new Scanner(System.in);
+  System.out.println("enter number");
+  int n=sc.nextInt();
+  System.out.println("enter number of digits");
+  int k=sc.nextInt();
+  if(k<=
+  StringBuffer sb=new StringBuffer(n);
+  String s=sb.toString();
+  char c[]=s.toCharArray();
+  Arrays.sort(c);
+  sb=new StringBuffer();
+  for(int i=0;i<c.length;i++)
+   sb.append(c[i]);
+  sb.delete(sb.length()-k,sb.length());
+  System.out.println(sb);
+ }
+} 
